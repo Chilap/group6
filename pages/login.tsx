12e467da67a60c7abd12c7fbb90d922abc3cccee
Chilap <cheng.chilap.221@gmail.com>
2021-11-29 22:35:12 +0800

    login completed

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,123 +1,137 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 import React, { useState } from "react";
 
-
 interface Item {
-    Programme: string,
-    ID: string,
-    Year: number,
+  Programme: string;
+  ID: string;
+  Year: number;
 }
 
 const IDPW: Item[] = [
-    {
-        Programme: "a",
-        ID: 'a',
-        Year: 1,
-    },
-    {
-        Programme: "AHCC",
-        ID: 'z',
-        Year: 1,
-    },
-    {
-        Programme: "DSBI",
-        ID: 'a',
-        Year: 1,
-    }
+  { ID: "a", Programme: "a", Year: 1 },
+  { ID: "z", Programme: "AHCC", Year: 1 },
+  { ID: "a", Programme: "DSBI", Year: 1 },
 ];
 
+const LoginBlock = () => {
+  const [loggedin, setLogin] = useState<boolean>(false);
+  const [input, setInput] = useState<Item>({
+    Programme: "",
+    ID: "",
+    Year: null,
+  });
+  //   const [result, setResult] = useState<string>("");
+  const { Programme, ID, Year } = input;
 
-
-  
-
-function login () {
-    const [input, setInput] = useState<Item>({
-        Programme:"",
-        ID: '',
-        Year: null,
-      });
-      const [result, setResult] = useState<string>('');
-      const { Programme ,ID, Year} = input;
-    
-      function check() {
-        for(const x in IDPW){
-  
-            if(ID==IDPW[x].ID && Programme==IDPW[x].Programme && Year==IDPW[x].Year){
-            {return(1)}
-            }   
-  
-            if(ID==IDPW[x].ID || Programme==IDPW[x].Programme || Year==IDPW[x].Year){
-            {return(0)}
-            }   
-    
-        }
+  function check() {
+    for (const x in IDPW) {
+      if (
+        ID == IDPW[x].ID &&
+        Programme == IDPW[x].Programme &&
+        Year == IDPW[x].Year
+      ) {
+        setLogin(true);
+        console.log(loggedin);
+        return true;
+      } else {
+        setLogin(false);
+        console.log(loggedin);
+        return false;
+      }
     }
-  
-    function popup() {
-        if(check()==1){
-            {return(alert("OK"))}
-        }
-        else{
-            {return(alert("Sorry, worng ID or password"))}
-        }
-        
-    }
+  }
 
-    function checklogin() {
-        let checklogin = []
-        checklogin.unshift(0)
-      
-        if(check()==1){
-            checklogin.unshift(1)
-            return(checklogin[0])
-        }
+  //   function popup() {
+  //     if (loggedin) {
+  //       {
+  //         console.log(loggedin);
+  //         return alert("OK");
+  //       }
+  //     } else {
+  //       {
+  //         console.log(loggedin);
+  //         return alert("Sorry, worng ID or password");
+  //       }
+  //     }
+  //   }
 
+  //   function checklogin() {
+  //     let checklogin = [];
+  //     checklogin.unshift(0);
 
-        
-    }
-    return(
-        
+  //     if (loggedin) {
+  //       checklogin.unshift(1);
+  //       return checklogin[0];
+  //     }
+  //   }
+  return (
+    <div>
+      {loggedin ? (
         <div>
-        <form>
-        <p><label>Student ID</label>
-        <input value={ID}onChange={x => setInput({...input, ID:(x.target.value)})} type="text"  required/>
-        </p>
-        <p><label>Programme</label>
-        <input value={Programme}onChange={x => setInput({...input, Programme:(x.target.value)})} type="text"  required/>
-        </p>
-        <p><label>Year   </label><br/>
-        <input value={Year}onChange={x => setInput({...input, Year:parseInt(x.target.value)})}
-            type="number"  min="1" max="5"/>
-        </p>
-        <p>
-        <button  className={styles.button}type = "submit" onClick={popup}>Log in </button></p>
-        </form>
-        </div>)
- 
-}
-  
- function logined() {
-    return(
-        
+          <p>Hello</p>
+          <button
+            className="bg-blue-200 text-gray-600 inline-block rounded-md px-4 py-2 text-sm my-2"
+            onClick={() => setLogin(false)}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
         <div>
-            Welcome!
-        </div>)
+          <p>
+            <label>Student ID</label>
+            <input
+              value={ID}
+              onChange={(x) => setInput({ ...input, ID: x.target.value })}
+              type="text"
+              required
+              // className = 'bg-transparent'
+            />
+          </p>
+          <p>
+            <label>Programme</label>
+            <input
+              value={Programme}
+              onChange={(x) =>
+                setInput({ ...input, Programme: x.target.value })
+              }
+              type="text"
+              required
+            />
+          </p>
+          <p>
+            <label>Year </label>
+            <br />
+            <input
+              value={Year}
+              onChange={(x) =>
+                setInput({ ...input, Year: parseInt(x.target.value) })
+              }
+              type="number"
+              min="1"
+              max="5"
+            />
+          </p>
+          <p>
+            <button
+              className="bg-blue-200 text-gray-600 inline-block rounded-md px-4 py-2 text-sm my-2"
+              type="submit"
+              onClick={() => {
+                check();
+                console.log(loggedin);
+              }}
+            >
+              Log in{" "}
+            </button>
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
 
-
-    
+function logined() {
+  return <div>Welcome!</div>;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-export default login
-  
-  
+export default LoginBlock;
