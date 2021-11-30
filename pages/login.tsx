13ac9f5da39d785f61e7ad6f5847ae602fa25a24
Chilap <cheng.chilap.221@gmail.com>
2021-11-30 19:49:56 +0800

    register to localstorage

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,3 @@
-import styles from "../styles/Home.module.css";
 import React, { useState } from "react";
 import credentials from "./credentials.json";
 
@@ -7,7 +6,9 @@ interface Item {
   Programme: string;
   Year: number;
 }
-
+// if (localStorage.getItem("credentials") == null) {
+// localStorage.setItem("credentials", JSON.stringify(credentials));
+// }
 const LoginBlock = () => {
   const [loggedin, setLogin] = useState<boolean>(false);
   const [input, setInput] = useState<Item>({
@@ -18,33 +19,71 @@ const LoginBlock = () => {
   const { ID, Programme, Year } = input;
 
   const check = () => {
-    for (const x in credentials) {
+    if (localStorage.getItem("credentials") == null) {
+      localStorage.setItem("credentials", JSON.stringify(credentials));
+    }
+    for (const x in JSON.parse(localStorage.getItem("credentials"))) {
+      // console.log(JSON.parse(credentialsShorthand));
+      console.log("0:", JSON.parse(localStorage.getItem("credentials"))[0].ID);
       if (
-        ID == credentials[x].ID &&
-        Programme == credentials[x].Programme &&
-        Year == credentials[x].Year
+        ID == JSON.parse(localStorage.getItem("credentials"))[x].ID &&
+        Programme ==
+          JSON.parse(localStorage.getItem("credentials"))[x].Programme &&
+        Year == JSON.parse(localStorage.getItem("credentials"))[x].Year
       ) {
         setLogin(true);
-        console.log(loggedin);
+        console.log(input);
+        console.log("hello");
         return true;
       } else {
         setLogin(false);
-        console.log(loggedin);
-        return false;
+        console.log("now: ", x);
+        console.log(credentials[x]);
+
       }
     }
   };
+
+  const register = () => {
+    if (localStorage.getItem("credentials") == null) {
+      localStorage.setItem("credentials", JSON.stringify(credentials));
+    }
+    for (const x in JSON.parse(localStorage.getItem("credentials"))) {
+      if (input.ID == JSON.parse(localStorage.getItem("credentials"))[x].ID) {
+        alert("ID already exists");
+        return;
+      }
+    }
+    const ls = JSON.parse(localStorage.getItem("credentials"));
+    ls.push(input);
+    localStorage.setItem("credentials", JSON.stringify(ls));
+
+    // localStorage.setItem("credentials", JSON.stringify([credentials, input]));
+    console.log(ID, Programme, Year);
+  };
   return (
     <div>
+      {/* {localStorage.setItem("credentials", JSON.stringify(credentials))} */}
+
       {loggedin ? (
         <div>
-          <p>Welcome!</p>
+          <p>Welcome! {input.ID}</p>
           <button
             className="bg-blue-200 text-gray-600 inline-block rounded-md px-4 py-2 text-sm my-2 hover:bg-blue-300 hover:text-black hover:shadow-xl transition duration-200"
             onClick={() => setLogin(false)}
           >
             Logout
           </button>
+          {input.ID == "doctor" ? (
+            <button
+              className="bg-blue-200 text-gray-600 inline-block rounded-md px-4 py-2 text-sm m-2 hover:bg-blue-300 hover:text-black hover:shadow-xl transition duration-200"
+              onClick={() => setLogin(false)}
+            >
+              Generate XML
+            </button>
+          ) : (
+            <> </>
+          )}
         </div>
       ) : (
         <div>
@@ -73,11 +112,11 @@ const LoginBlock = () => {
             />
           </p>
           <p>
-            <label>Year </label>
+            <label>Year of entrance</label>
             <br />
             <input
               value={Year}
-              placeholder="1"
+              placeholder="2019"
               onChange={(x) =>
                 setInput({ ...input, Year: parseInt(x.target.value) })
               }
@@ -97,6 +136,15 @@ const LoginBlock = () => {
               }}
             >
               Log in{" "}
+            </button>
+            <button
+              className="bg-blue-200 text-gray-600 inline-block rounded-md px-4 py-2 text-sm m-2 hover:bg-blue-300 hover:text-black hover:shadow-xl transition duration-200"
+              type="submit"
+              onClick={() => {
+                register();
+              }}
+            >
+              Register{" "}
             </button>
           </p>
         </div>
