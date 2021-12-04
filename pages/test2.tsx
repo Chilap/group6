fd6ddc32a75361190c8b13aeea48bc1c92bd84d6
douglas <gundamseed02lam@gmail.com>
2021-12-04 15:43:52 +0800

    Update test2.tsx
    version 1

--- pages/test2.tsx
+++ pages/test2.tsx
@@ -38,6 +38,75 @@ const load = () => {
     localStorage.setItem("grades", JSON.stringify(grades));
   }
 };
+export const GradeToGPA: Record<GradeType, number> = {
+  ['A']: 4,
+  ['A-']: 3.7,
+  ['B+']: 3.3,
+  ['B']: 3,
+  ['B-']: 2.7,
+  ['C+']: 2.3,
+  ['C']: 2,
+  ['C-']: 1.7,
+  ['D+']: 1.3,
+  ['D']: 1,
+  ['F']: 0,
+}
+const Modules = [
+  'COM3102',
+  'COM3103',
+  'AMS1360',
+  'AMS2640',
+  'COM2005',
+  'COM3104',
+] as const;
+type module = typeof Modules[number];
+export const GradeDatabase: Record<module, string> = {
+  ['COM3102']:'B+',
+  ['COM3103']:'B-',
+  ['AMS1360']:'A-',
+  ['AMS2640']:'B',
+  ['COM2005']: 'C+',
+  ['COM3104']: 'B'
+}  
+export interface StudentTransscript {
+  Programme: string; 
+  results: string;
+}  
+
+function calculate(x:string, z:string) {
+  if (x === GradeDatabase[x]) {
+      let results = 0;
+      let average = 0
+      var y:string;
+      results = GradeToGPA[GradeDatabase[x]]+GradeToGPA[z]
+      average = results / 2
+      if (average <= 4 && average > 3.7){
+        y = 'A'
+      } else if (average <= 3.7 && average > 3.3){
+        y = 'A-'
+      } else if (average <= 3.3 && average > 3){
+        y = 'B+'
+      } else if (average <= 3 && average > 2.7){
+        y = 'B'
+      } else if (average <= 2.7 && average > 2.3){
+        y = 'B-'
+      } else if (average <= 2.3 && average > 2){
+        y = 'C+'
+      } else if (average <= 2 && average > 2.7){
+        y = 'C'
+      } else if (average <= 2.7 && average > 2.3){
+        y = 'C-'
+      } else if (average <= 2.3 && average > 1){
+        y = 'D+'
+      } else if (average = 1){
+        y = 'D'
+      } else {
+        y = 'F'
+      }
+      return y;
+  }
+  console.log(`The average grade of ${GradeDatabase[x]} is ${y}`)
+}
 const SubmitScore2 = () => {
   const [data, setData] = useState<Item>({ Programme_code: "", grade: "" });
   const [initiated, setInitiated] = useState<boolean>(false);
@@ -122,6 +191,10 @@ const SubmitScore2 = () => {
           >
             Clear local storage
           </button>
+          <button
+            onClick={calculate(data.Programme_code, data.grade)}
+            className="bg-blue-200 mx-2 text-gray-600 inline-block rounded-md px-4 py-2 text-sm my-2 hover:bg-blue-300 hover:text-black hover:shadow-xl transition duration-200"
+          ></button>
           <table className={styles.table}>
             <tr className={styles.tr}>
               <th className={styles.th}>Programme Code</th>
